750861ebaa02f5ca5c629eed3127b68a67f810f6
Karim Aouadi <kdolphinus@gmail.com>
2020-02-22 12:02:18 +0100

    updated main

--- anesthesis/main.js
+++ anesthesis/main.js
@@ -1,1 +1,35 @@
 console.log('hello');
+
+
+class A{
+    constructor(){
+
+
+    }
+    add(){
+
+        Object.defineProperty(this, 'amount', {
+            value: 42,
+            writable: false
+          });
+          
+
+    }
+    display(){
+
+        console.log(`${this.amount}`)
+    }
+}
+
+const objA = new A();
+console.log(objA.add());
+console.log(objA.amount);
+objA.display();
+
+let str = `
+${console.log('Bonjour')}
+`
+
+
+
+
